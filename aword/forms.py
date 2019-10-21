7ddb0f4c723e87e86af05ff396a512ab8b5c5a44
michael760odhiambo <michaelodhiambo760@gmail.com>
2019-10-21 22:51:12 +0300

    added signup form

--- aword/forms.py
+++ aword/forms.py
@@ -18,11 +18,11 @@ class ProjectForm(forms.ModelForm):
             'countries':forms.CheckboxSelectMultiple(),
         }
 
-# class SignUpForm(UserCreationForm):
-#     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-#     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-#     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
 
-#     class Meta:
-#         model = User
-#         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )        
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )        
