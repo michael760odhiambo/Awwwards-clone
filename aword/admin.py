773011ee2636bd9608e15ef3c200c50f9779d777
michael760odhiambo <michaelodhiambo760@gmail.com>
2019-10-21 22:51:52 +0300

    added my models to the admin page

--- aword/admin.py
+++ aword/admin.py
@@ -1,14 +1,14 @@
-# from django.contrib import admin
-# from .models import Categories,Technologies,Countries,Project,Profile
+from django.contrib import admin
+from .models import Categories,Technologies,Countries,Project,Profile
 
 
 
-# # Register your models here.
-# admin.site.register(Categories)
-# admin.site.register(Technologies)
+# Register your models here.
+admin.site.register(Categories)
+admin.site.register(Technologies)
 
-# admin.site.register(Countries)
-# admin.site.register(Project)
-# admin.site.register(Profile)
+admin.site.register(Countries)
+admin.site.register(Project)
+admin.site.register(Profile)
 
-# # Register your models here.
+# Register your models here.
