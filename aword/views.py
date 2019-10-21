fafb565f5d2e61fa64d0d6db833e8e3f1644702e
michael760odhiambo <michaelodhiambo760@gmail.com>
2019-10-21 22:46:33 +0300

    added signup function

--- aword/views.py
+++ aword/views.py
@@ -97,19 +97,19 @@ def search_results(request):
         message="You haven't searched for any term"
         return render(request,'all-pages/search.html',{"message":message})
 
-# def signup(request):
-#     if request.method == 'POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             username = form.cleaned_data.get('username')
-#             raw_password = form.cleaned_data.get('password1')
-#             user = authenticate(username=username, password=raw_password)
-#             login(request, user)
-#             return redirect('homepage')
-#     else:
-#         form = UserCreationForm()
-#     return render(request, 'registration/signup.html', {'form': form})        
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('homepage')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})        
 # # Create your views here.
 
 
