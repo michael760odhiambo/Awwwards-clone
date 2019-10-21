84808ac8a4e8e19c39ea26b2a551ff6b4c386bf8
michael760odhiambo <michaelodhiambo760@gmail.com>
2019-10-21 22:47:07 +0300

    added urls

--- aword/urls.py
+++ aword/urls.py
@@ -1,23 +1,23 @@
-# from django.urls import path
-# from . import views
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
-# urlpatterns = [
-#     path('', views.home, name='homepage'),
-#     path('create/profile',views.create_profile, name='create-profile'),
-#     path('new/project',views.new_project, name='new-project'),
-#     path('profile/',views.profile, name='profile'),
-#     path('search/',views.search_results, name='search_results'),
-#     #path('user/',views.user_profile,name='user-profile'),
-#     path('signup/',views.signup, name='signup'),
-#     path('api/profiles/', views.ProfileList.as_view()),
-#     path('api/projects/', views.ProjectList.as_view()),
-#     path('api/categories/', views.CategoriesList.as_view()),
-#     path('api/countries/', views.CountriesList.as_view()),
-#     path('api/technologies/', views.TechnologiesList.as_view()),
+urlpatterns = [
+    path('', views.home, name='homepage'),
+    path('create/profile',views.create_profile, name='create-profile'),
+    path('new/project',views.new_project, name='new-project'),
+    path('profile/',views.profile, name='profile'),
+    path('search/',views.search_results, name='search_results'),
+    #path('user/',views.user_profile,name='user-profile'),
+    path('signup/',views.signup, name='signup'),
+    path('api/profiles/', views.ProfileList.as_view()),
+    path('api/projects/', views.ProjectList.as_view()),
+    path('api/categories/', views.CategoriesList.as_view()),
+    path('api/countries/', views.CountriesList.as_view()),
+    path('api/technologies/', views.TechnologiesList.as_view()),
     
 
-# ]
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
