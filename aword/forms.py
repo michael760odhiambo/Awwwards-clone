8ee4973b09f7bd9b571d9a99da2d055d7962fd62
michael760odhiambo <michaelodhiambo760@gmail.com>
2019-10-21 22:50:26 +0300

    added profile form

--- aword/forms.py
+++ aword/forms.py
@@ -1,12 +1,12 @@
-# from django import forms
-# from django.contrib.auth.forms import UserCreationForm
-# from .models import Profile,Project
-# from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile,Project
+from django.contrib.auth.models import User
 
-# class ProfileForm(forms.ModelForm):
-#     class Meta:
-#         model = Profile
-#         exclude = ['username']
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['username']
 
 # class ProjectForm(forms.ModelForm):
 #     class Meta:
