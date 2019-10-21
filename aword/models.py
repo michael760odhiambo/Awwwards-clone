4f66f1870892c3830eb41425ca0590ee8ab6986e
michael760odhiambo <michaelodhiambo760@gmail.com>
2019-10-21 22:48:15 +0300

    added serializer

--- aword/models.py
+++ aword/models.py
@@ -1,38 +1,38 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-# from tinymce.models import HTMLField
-# #from django.db.models import search_term
-# import datetime as dt
+from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+#from django.db.models import search_term
+import datetime as dt
 
 
 
 
 
-# class Categories(models.Model):
-#     categories = models.CharField(max_length=40)
+class Categories(models.Model):
+    categories = models.CharField(max_length=40)
 
-#     def __str__(self):
-#         return self.categories
+    def __str__(self):
+        return self.categories
 
-#     def save_category(self):
-#         self.save()
+    def save_category(self):
+        self.save()
 
-#     @classmethod
-#     def delete_category(cls, categories):
-#         cls.objects.filter(categories=categories).delete()
+    @classmethod
+    def delete_category(cls, categories):
+        cls.objects.filter(categories=categories).delete()
 
-# class Technologies(models.Model):
-#     technologies = models.CharField(max_length=40)
+class Technologies(models.Model):
+    technologies = models.CharField(max_length=40)
 
-#     def __str__(self):
-#         return self.technologies
+    def __str__(self):
+        return self.technologies
 
-#     def save_technologies(self):
-#         self.save()
+    def save_technologies(self):
+        self.save()
 
-#     @classmethod
-#     def delete_technologies(cls, technologies):
-#         cls.objects.filter(technologies=technologies).delete()
+    @classmethod
+    def delete_technologies(cls, technologies):
+        cls.objects.filter(technologies=technologies).delete()
 
 # class Countries(models.Model):
 #     countries = models.CharField(max_length=40)
